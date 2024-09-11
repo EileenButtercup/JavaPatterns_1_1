cdca8aa18cc09d71fc540f5c70187024d0b18e96
EileenButtercup <165425510+EileenButtercup@users.noreply.github.com>
2024-09-11 20:07:54 +0300

    Update Person.java

--- Person.java
+++ Person.java
@@ -4,18 +4,15 @@ import java.util.OptionalInt;
 public class Person {
     protected final String name;
     protected final String surname;
-    protected int age;
+
+    protected Integer age;
     protected String address;
 
-    public Person(String name, String surname) {
-        this.name = name;
-        this.surname = surname;
-    }
-
-    public Person(String name, String surname, int age) {
-        this.name = name;
-        this.surname = surname;
-        this.age = age;
+    public Person(PersonBuilder personBuilder) {
+        this.name = personBuilder.name;
+        this.surname = personBuilder.surname;
+        this.age = personBuilder.age;
+        this.address = personBuilder.address;
     }
 
     public boolean hasAge() {
@@ -23,7 +20,10 @@ public class Person {
     }
 
     public boolean hasAddress() {
-        return address != null;
+        if (address == null) {
+            return false;
+        }
+        return true;
     }
 
     public String getName() {
@@ -42,35 +42,25 @@ public class Person {
         return address;
     }
 
-    public String setAddress(String address) {
-        this.address = address;
-        return this.address;
-    }
-
     public void happyBirthday() {
         if (hasAge()) age++;
     }
 
     public PersonBuilder newChildBuilder() {
-        return new PersonBuilder().setSurname(surname).setAddress(address);
+        PersonBuilder personBuilder = new PersonBuilder();
+        personBuilder.setSurname(getSurname());
+        personBuilder.setAddress(getAddress());
+        return personBuilder;
     }
 
     @Override
     public String toString() {
-        return "Person{" +
-                "name='" + name + '\'' +
-                ", surname='" + surname + '\'' +
-                ", age=" + age +
-                ", address='" + address + '\'' +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Person person = (Person) o;
-        return age == person.age && name.equals(person.name) && surname.equals(person.surname) && Objects.equals(address, person.address);
+        StringBuilder builder = new StringBuilder();
+        builder.append("[name= ").append(name)
+                .append(", surname= ").append(surname)
+                .append(", age= ").append(age)
+                .append(", address= ").append(address).append("]");
+        return builder.toString();
     }
 
     @Override
